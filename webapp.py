56c7ba7838698a5312acc87881534fcaf997ad37
adkinsrc <adkinsrc@jmu.edu>
2020-07-01 18:51:36 -0400

    Cosmetic changes to home page and app review page. App preview added to app review page. Favicon icon file added, but not yet working in browser.

--- webapp.py
+++ webapp.py
@@ -8,17 +8,17 @@ app.config['SECRET_KEY'] = "someth1ng super secret and maybe even rand0m"
 
 app_reviews = {
     "1": {
-        "title": "reliefQ",
-        "slogan": "see what matters.",
-        "icon": "heart.png",
-        "description": "reliefQueue raises funds from its charitable users for the causes currently in the most need. Organizations create a profile with reliefQueue and share statistics such as humanitarian impact, charitable commitment, fundraising efficiency, and current demand of their cause/charity. Our algorithm measures and compares these factors and promotes charities of the most need to the top of the reliefQueue. Organizations of exceptional need can be “prompted” to the top of reliefQueue by developers.",
-        "next": "2"
-    },
-    "2": {
         "title": "Lorem ipsum",
         "slogan": "There is no one who loves pain itself",
         "icon": "hurt.png",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec vehicula felis. Suspendisse sollicitudin ante lacus, ac mattis odio elementum et. Vivamus sit amet efficitur odio. Sed commodo auctor massa, in eleifend lectus porta sed. Maecenas id massa eleifend, tincidunt mi sed, sagittis erat. Nullam volutpat, arcu a rhoncus ultrices, lectus libero hendrerit odio, sed vehicula nisl dolor ac lectus. Praesent posuere eros quis ligula pharetra facilisis. Duis dictum eu dui eu sagittis. Suspendisse et risus in nunc imperdiet ultricies. Nam at augue quis enim venenatis fermentum eu accumsan neque. Maecenas porta mauris massa, sit amet commodo augue auctor sed. Donec congue at libero at tincidunt. Sed tristique ac diam ut convallis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam ut felis sit amet nunc tincidunt vestibulum. Mauris rhoncus mattis volutpat. Nulla vitae lacus et lacus porttitor lacinia. Etiam non ante ut elit cursus tincidunt. Aenean nec nulla pulvinar, dignissim eros nec, consectetur lorem. Suspendisse iaculis tortor id nibh consequat, eget efficitur enim fermentum. Sed ac mi sit amet dui cursus volutpat sed id nulla. Maecenas malesuada neque vitae orci efficitur bibendum. Vivamus ac nisi posuere elit egestas varius vitae ac lacus",
+        "next": "2"
+    },
+    "2": {
+        "title": "reliefQ",
+        "slogan": "see what matters.",
+        "icon": "heart.png",
+        "description": "reliefQ raises funds from its charitable users for the causes currently in the most need. Organizations create a profile with reliefQ and share statistics such as humanitarian impact, charitable commitment, fundraising efficiency, and current demand of their cause/charity. Our algorithm measures and compares these factors and promotes charities of the most need to the top of the reliefQ. Organizations of exceptional need can be “prompted” to the top of reliefQ by developers.",
         "next": "3"
     },
     "3": {
