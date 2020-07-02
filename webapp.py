4e7d60f8f76aecf25beaa30b5e0f58a423ad29f5
adkinsrc <adkinsrc@jmu.edu>
2020-07-01 21:16:47 -0400

    Created new app concept based on facial recognition tech used by police. Changed colors/aesthetic on home page a little bit. Results page now shows after final app. Field added to app_review class in webapp.py to hold approved or rejected status, but not implemented to work.

--- webapp.py
+++ webapp.py
@@ -8,25 +8,58 @@ app.config['SECRET_KEY'] = "someth1ng super secret and maybe even rand0m"
 
 app_reviews = {
     "1": {
-        "title": "Lorem ipsum",
-        "slogan": "There is no one who loves pain itself",
-        "icon": "hurt.png",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec vehicula felis. Suspendisse sollicitudin ante lacus, ac mattis odio elementum et. Vivamus sit amet efficitur odio. Sed commodo auctor massa, in eleifend lectus porta sed. Maecenas id massa eleifend, tincidunt mi sed, sagittis erat. Nullam volutpat, arcu a rhoncus ultrices, lectus libero hendrerit odio, sed vehicula nisl dolor ac lectus. Praesent posuere eros quis ligula pharetra facilisis. Duis dictum eu dui eu sagittis. Suspendisse et risus in nunc imperdiet ultricies. Nam at augue quis enim venenatis fermentum eu accumsan neque. Maecenas porta mauris massa, sit amet commodo augue auctor sed. Donec congue at libero at tincidunt. Sed tristique ac diam ut convallis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam ut felis sit amet nunc tincidunt vestibulum. Mauris rhoncus mattis volutpat. Nulla vitae lacus et lacus porttitor lacinia. Etiam non ante ut elit cursus tincidunt. Aenean nec nulla pulvinar, dignissim eros nec, consectetur lorem. Suspendisse iaculis tortor id nibh consequat, eget efficitur enim fermentum. Sed ac mi sit amet dui cursus volutpat sed id nulla. Maecenas malesuada neque vitae orci efficitur bibendum. Vivamus ac nisi posuere elit egestas varius vitae ac lacus",
+        "title": "City of Harrisonburg Police Department Warrant Planner",
+        "slogan": "Integrity, Accountability, Honor, Leadership, Diversity",
+        "icon": "warrant_icon.png",
+        "preview": "warrant_preview.png",
+        "description": "This app is only for use by liscensed officers of the City of Harrisonburg Police Department and may " +
+                        "only be downloaded with affirmed consent from the City of Harrisonburg Police Department." +
+                        "This app will be used to expedite the search for and booking of convicted criminals in the City of " +
+                        "Harrisonburg area. When a warrant is issued for an individual's arrest, their description and identifying " +
+                        "characteristics are automatically entered in to the app. Harrisonburg's facial detection system, enabled by " +
+                        "traffic cameras on our major streets, will then be able to automatically locate, flag, and track any " +
+                        "individual matching the description when they are seen by a camera. Officers logged in to the app " +
+                        "can see the status of an arrest warrant and the location of any suspects. This innovative app will make " +
+                        "for a more efficient police department and a safer City of Harrisonburg.",
+        "result": "",
         "next": "2"
     },
     "2": {
         "title": "reliefQ",
         "slogan": "see what matters.",
-        "icon": "heart.png",
-        "description": "reliefQ raises funds from its charitable users for the causes currently in the most need. Organizations create a profile with reliefQ and share statistics such as humanitarian impact, charitable commitment, fundraising efficiency, and current demand of their cause/charity. Our algorithm measures and compares these factors and promotes charities of the most need to the top of the reliefQ. Organizations of exceptional need can be “prompted” to the top of reliefQ by developers.",
+        "icon": "reliefQ_icon.png",
+        "preview": "reliefQ_preview.png",
+        "description": "reliefQ raises funds from its charitable users for the causes currently in the most need. Organizations "
+                        + "create a profile with reliefQ and share statistics such as humanitarian impact, charitable commitment, "
+                        + " fundraising efficiency, and current demand of their cause/charity. Our algorithm measures and compares "
+                        + "these factors and promotes charities of the most need to the top of the reliefQ. Organizations of "
+                        + "exceptional need can be “prompted” to the top of reliefQ by developers.",
+        "result": "",
         "next": "3"
     },
     "3": {
-        "title": "Aaaarrrrgggghhhh!",
-        "slogan": "cleave him to the brisket!!!!!!!",
-        "icon": "pirate.png",
-        "description": "What in Davy Jones’ locker did ye just bark at me, ye scurvy bilgerat? I’ll have ye know I be the meanest cutthroat on the seven seas, and I’ve led numerous raids on fishing villages, and [censored] over 300 wenches. I be trained in hit-and-run pillaging and be the deadliest with a pistol of all the captains on the high seas. Ye be nothing to me but another source o’ swag. I’ll have yer guts for garters and keel haul ye like never been done before, hear me true. You think ye can hide behind your newfangled computing device? Think twice on that, scallywag. As we parley I be contacting my secret network o’ pirates across the sea and yer port is being tracked right now so ye better prepare for the typhoon, weevil. The kind o’ monsoon that’ll wipe ye off the map. You’re sharkbait, fool. I can sail anywhere, in any waters, and can kill ye in o’er seven hundred ways, and that be just with me hook and fist. Not only do I be top o’ the line with a cutlass, but I have an entire pirate fleet at my beck and call and I’ll [censored] sure use it all to wipe yer [censored] off o’ the world, ye dog. If only ye had had the foresight to know what devilish wrath your jibe was about to incur, ye might have belayed the comment. But ye couldn’t, ye didn’t, and now ye’ll pay the ultimate toll, you buffoon. I’ll [censored] fury all over ye and ye’ll drown in the depths o’ it. You’re fish food now.",
-        "next": "3"
+        "title": "Lorem ipsum",
+        "slogan": "There is no one who loves pain itself",
+        "icon": "placeholder_icon.png",
+        "preview": "placeholder.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec vehicula felis. "
+                        + "Suspendisse sollicitudin ante lacus, ac mattis odio elementum et. Vivamus sit "
+                        + "amet efficitur odio. Sed commodo auctor massa, in eleifend lectus porta sed. Maecenas "
+                        + "id massa eleifend, tincidunt mi sed, sagittis erat. Nullam volutpat, arcu a rhoncus "
+                        + "ultrices, lectus libero hendrerit odio, sed vehicula nisl dolor ac lectus. Praesent "
+                        + "posuere eros quis ligula pharetra facilisis. Duis dictum eu dui eu sagittis. Suspendisse "
+                        + "et risus in nunc imperdiet ultricies. Nam at augue quis enim venenatis fermentum eu "
+                        + "accumsan neque. Maecenas porta mauris massa, sit amet commodo augue auctor sed. Donec "
+                        + "congue at libero at tincidunt. Sed tristique ac diam ut convallis. Class aptent taciti "
+                        + "sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam ut felis sit "
+                        + "amet nunc tincidunt vestibulum. Mauris rhoncus mattis volutpat. Nulla vitae lacus et "
+                        + "lacus porttitor lacinia. Etiam non ante ut elit cursus tincidunt. Aenean nec nulla pulvinar, "
+                        + "dignissim eros nec, consectetur lorem. Suspendisse iaculis tortor id nibh consequat, eget "
+                        + "efficitur enim fermentum. Sed ac mi sit amet dui cursus volutpat sed id nulla. Maecenas "
+                        + "malesuada neque vitae orci efficitur bibendum. Vivamus ac nisi posuere elit egestas varius "
+                        + "vitae ac lacus.",
+        "result": "",
+        "next": "results"
     }
 }
 
@@ -79,6 +112,6 @@ def show_review(app_id="1", hash=None):
     
 #     return render_template("review.html", app_review=app_reviews[app_id])
 
-@app.route("/review/results", strict_slashes=False)
+@app.route("/review/app/results", strict_slashes=False)
 def results():
     return render_template("results.html", approved_apps=approved_apps, rejected_apps=rejected_apps)
